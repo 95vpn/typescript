87f0a9df10c0a7adba0677e2424957077430ac0e
Johnny Herrera <jmjaramillo95@gmail.com>
2025-03-18 23:41:47 -0500

    mappers

---
+++ advance/utilityTypes.ts
@@ -0,0 +1,12 @@
+interface Task {
+    id: number;
+    name: string;
+    description: string;
+}
+
+function updateTask(id: string, task: Task) {
+    //
+    console.log();
+}
+
+const newTask = { name: "Kevin" };
